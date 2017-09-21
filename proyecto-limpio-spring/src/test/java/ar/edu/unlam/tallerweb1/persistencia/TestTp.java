15cf16adbae0a203f9fb4941ee58bf7225b3959d
mquiroz25 <mquiroz2535@gmail.com>
2017-09-21 16:06:14 -0300

    agregados los tests

---
+++ proyecto-limpio-spring/src/test/java/ar/edu/unlam/tallerweb1/persistencia/TestTp.java
@@ -0,0 +1,158 @@
+package ar.edu.unlam.tallerweb1.persistencia;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Barrio;
+import ar.edu.unlam.tallerweb1.modelo.Direccion;
+import ar.edu.unlam.tallerweb1.modelo.Farmacia;
+
+public class TestTp  extends SpringTest{
+
+	@Test
+	@Transactional
+	@Rollback(true)
+public void TraerTodasLasFarmaciasDeTurnoEnLosDiasMartes() {
+		
+		Farmacia farmacia1 = new Farmacia("Vilela", "Martes");
+		Farmacia farmacia2 = new Farmacia("Morales", "Domingo");
+		Farmacia farmacia3 = new Farmacia("Inca", "Domingo");
+		Farmacia farmacia4 = new Farmacia("Ruiz", "Martes");
+		Farmacia farmacia5 = new Farmacia("Moyano", "Martes");
+		
+		getSession().save(farmacia1);
+		getSession().save(farmacia2);
+		getSession().save(farmacia3);
+		getSession().save(farmacia4);
+		getSession().save(farmacia5);
+		
+		List<Farmacia> farmacias= getSession().createCriteria(Farmacia.class)
+				.add(Restrictions.eq("diaDeTurno", "Martes")).list();
+
+	for(int i=0;i<farmacias.size();i++)
+		{
+			assertTrue(farmacias.get(i).getDiaDeTurno()=="Martes");
+		}
+}
+
+	@Test
+	@Transactional
+	@Rollback(true)
+	
+public void TraerTodasLasFarmaciasDeUnaCalle() {
+		
+		Barrio barrio1=new Barrio("Flores");
+		Barrio barrio2=new Barrio("San Justo");
+		
+		getSession().save(barrio1);
+		getSession().save(barrio2);
+		
+		
+		Direccion direccion1=new Direccion("Int.Russo","5827",barrio1);
+		Direccion direccion2=new Direccion("Int.Russo","3050",barrio1);
+		Direccion direccion3=new Direccion("Arieta","1000",barrio2);
+		
+		getSession().save(direccion1);
+		getSession().save(direccion2);
+		getSession().save(direccion3);
+	
+		Farmacia farmacia1 = new Farmacia("Vilela", "Martes");
+		Farmacia farmacia2 = new Farmacia("Morales", "Domingo");
+		Farmacia farmacia3 = new Farmacia("Inca", "Domingo");
+		
+		
+		farmacia1.setDireccion(direccion1);
+		farmacia2.setDireccion(direccion2);
+		farmacia3.setDireccion(direccion3);
+		
+		getSession().save(farmacia1);
+		getSession().save(farmacia2);
+		getSession().save(farmacia3);
+
+		List<Farmacia> farmacias = getSession().createCriteria(Farmacia.class)
+				.createAlias("direccion", "dir")
+				.add(Restrictions.eq("dir.calle", "Int.Russo"))
+				.list();
+		
+		
+	
+
+	for(int i=0;i<farmacias.size();i++)
+		{
+			assertTrue(farmacias.get(i).getDireccion().getCalle()=="Int.Russo");
+		}
+}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	
+public void TraerTodasLasFarmaciasDeUnBarrio() {
+		
+		Barrio barrio1=new Barrio("Flores");
+		Barrio barrio2=new Barrio("San Justo");
+		Barrio barrio3=new Barrio("Pompeya");
+		
+		getSession().save(barrio1);
+		getSession().save(barrio2);
+		getSession().save(barrio3);
+		
+		Direccion direccion1=new Direccion("Int.Russo","5827",barrio1);
+		Direccion direccion2=new Direccion("Int.Russo","3050",barrio1);
+		Direccion direccion3=new Direccion("Indarte","1500",barrio1);
+		Direccion direccion4=new Direccion("Arieta","1000",barrio2);
+		Direccion direccion5=new Direccion("Saenz","2000",barrio3);
+		
+		getSession().save(direccion1);
+		getSession().save(direccion2);
+		getSession().save(direccion3);
+		getSession().save(direccion4);
+		getSession().save(direccion5);
+		
+	
+	
+		Farmacia farmacia1 = new Farmacia("Vilela", "Martes");
+		Farmacia farmacia2 = new Farmacia("Morales", "Domingo");
+		Farmacia farmacia3 = new Farmacia("Inca", "Domingo");
+		Farmacia farmacia4 = new Farmacia("Ruiz", "Martes");
+		Farmacia farmacia5 = new Farmacia("Moyano", "Martes");
+		
+		farmacia1.setDireccion(direccion1);
+		farmacia2.setDireccion(direccion2);
+		farmacia3.setDireccion(direccion3);
+		farmacia4.setDireccion(direccion4);
+		farmacia5.setDireccion(direccion5);
+		
+		getSession().save(farmacia1);
+		getSession().save(farmacia2);
+		getSession().save(farmacia3);
+		getSession().save(farmacia4);
+		getSession().save(farmacia5);
+		
+		
+		List<Farmacia> farmacias = getSession().createCriteria(Farmacia.class)
+				.createAlias("direccion", "dir")
+				.createAlias("dir.barrio", "bar")
+				.add(Restrictions.eq("bar.nombre", "Flores"))
+				.list();
+		
+		
+	for(int i=0;i<farmacias.size();i++)
+		{
+;
+		assertTrue(farmacias.get(i).getDireccion().getBarrio().getNombre()=="Flores");
+		}
+}
+	
+	
+}
